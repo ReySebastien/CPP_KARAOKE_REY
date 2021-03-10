a020bfef1dfa6ff15efa3ade5e9c9a8f915fd4b0
Sébastien Rey <rey.babou35@free.fr>
2021-03-10 10:13:44 +0100

    création joueurs

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main()
 {
-
+Player François = Player("François", 0, 0, 0, 0, 0);
+Player Amanda = Player("Amanda", 0, 0, 0, 0, 0);
+Player Julien = Player("Julien", 0, 0, 0, 0, 0);
+Player Arnaud = Player("Arnaud", 0, 0, 0, 0, 0);
+Player Sylvestre = Player("Sylvestre", 0, 0, 0, 0, 0);
 
 return 0;
 }
