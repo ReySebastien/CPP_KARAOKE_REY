a0340369c102fe90c282e58c7da69a6bb9cf3fe2
Sébastien Rey <rey.babou35@free.fr>
2021-03-10 10:46:00 +0100

    changement valeur score

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-Player Francois = Player("François", 1, 2, 3, 1, 1);
-Player Amanda = Player("Amanda", 1, 0, 0, 0, 0);
-Player Julien = Player("Julien", 0, 0, 0, 0, 0);
-Player Arnaud = Player("Arnaud", 0, 0, 0, 0, 0);
-Player Sylvestre = Player("Sylvestre", 0, 0, 0, 0, 0);
+Player Francois = Player("François", 66, 75, 88, 95, 50);
+Player Amanda = Player("Amanda", 55, 75, 59, 82, 74);
+Player Julien = Player("Julien", 55, 69, 50, 75, 100);
+Player Arnaud = Player("Arnaud", 98, 85, 69, 78, 51);
+Player Sylvestre = Player("Sylvestre", 78, 88, 99, 55, 67);
 
 Francois.afficheScoreTotal();
 Amanda.afficheScoreTotal();
