24ad36d5ee10bca26683c1ddd4ee5cdff7e4f43c
Sébastien Rey <rey.babou35@free.fr>
2021-03-10 09:36:32 +0100

    UML+player.h

--- Player.h
+++ Player.h
@@ -1,2 +1,22 @@
 #include <string>
 
+class Player
+{
+    private:
+        std::string _nom;
+        int _score0;
+        int _score1;
+        int _score2;
+        int _score3;
+        int _score4;
+
+    public:
+        std::string getNom();
+        int _getScore0();
+        int _getScore1();
+        int _getScore2();
+        int _getScore3();
+        int _getScore4();
+
+
+}
